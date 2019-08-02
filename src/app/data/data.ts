e404ce5512e8fffe3dec7fc057e5c6aee21ff631
Kate Turalnikova <turalnikova@gmail.com>
2019-08-02 22:43:08 +0300

    add movies to markup

---
+++ src/app/data/data.ts
@@ -0,0 +1,3 @@
+export const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+export const IMAGE_DEFAULT =
+  "https://lajoyalink.com/wp-content/uploads/2018/03/Movie.jpg";
--- src/app/movies/movie-list.component.ts
+++ src/app/movies/movie-list.component.ts
@@ -1,9 +1,25 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
+import data from "./movies.js";
+import { IMAGE_PATH, IMAGE_DEFAULT } from "../data/data";
+
+const { results } = data;
 
 @Component({
-    selector:"mm-movies",
-    templateUrl:"./movie-list.component.html"
+  selector: "mm-movies",
+  templateUrl: "./movie-list.component.html"
 })
-export class MovieListComponent{
-
+export class MovieListComponent {
+  pageTitle: string = "Movie List";
+  img_width: number = 150;
+  img_margin: number = 2;
+  tableHeader: string[] = [
+    "Movie",
+    "Language",
+    "Available",
+    "Popularity",
+    "Reviews"
+  ];
+  movies: any[] = results;
+  image_default = IMAGE_DEFAULT;
+  image_path = IMAGE_PATH;
 }
